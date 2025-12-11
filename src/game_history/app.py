ba7e2b56da1987d3efec828b98ac058cffbb7ba0
Federico Fornaciari <federico240701@gmail.com>
2025-12-11 10:14:41 +0100

    errmm...

--- src/game_history/app.py
+++ src/game_history/app.py
@@ -15,7 +15,7 @@ PAGE_SIZE = 10
 # --- User Service ---
 USER_MANAGER_URL = os.environ.get('USER_MANAGER_URL', 'https://user-manager:5000')
 USERNAMES_BY_IDS_URL = f'{USER_MANAGER_URL}/utenti/usernames-by-ids'
-USER_MANAGER_CERT = os.environ.get('USER_MANAGER_CERT', '/run/secrets/history_key')
+USER_MANAGER_CERT = os.environ.get('USER_MANAGER_CERT', '/run/secrets/user_manager_cert')
 
 # --- RabbitMQ broker ---
 RABBITMQ_HOST = os.environ.get("RABBITMQ_HOST", "rabbitmq")
